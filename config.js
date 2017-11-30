51c69486a17412988d938ae035edac424ce08089
harrypotter0 <9654263057akashkandpal@gmail.com>
2017-11-30 19:26:55 +0530

    Bot is up

---
+++ config.js
@@ -0,0 +1,7 @@
+// Put your own Twitter App keys here. See README.md for more detail.
+module.exports = {
+  consumer_key:         'bbzH2LpHjpS2F7zvYPazUMeQ6',
+  consumer_secret:      'ibwVzBJAkTFfC3OyIondKVI7lxUntCGfwYBdhNu72u2G7vBUV0',
+  access_token:         '936214979048435712-EiXO8dqe0NQcEBnQHKzSPdMRjgQLthb',
+  access_token_secret:  'HDdA3LAzj41sii7ymwZJXmZmPzdOh5KchnWWnDSTUOblo'
+}
